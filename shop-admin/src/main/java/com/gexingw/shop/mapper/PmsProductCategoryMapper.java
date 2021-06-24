3a32dc186047d7ee4c91c7cba143868a81b47a1b
WangSF <wolfs_9@hotmail.com>
2021-06-24 23:06:48 +0800

    sparkles: 添加后台商品管理代码（未完成）

---
+++ shop-admin/src/main/java/com/gexingw/shop/mapper/PmsProductCategoryMapper.java
@@ -0,0 +1,7 @@
+package com.gexingw.shop.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.gexingw.shop.bean.pms.PmsProductCategory;
+
+public interface PmsProductCategoryMapper extends BaseMapper<PmsProductCategory> {
+}
---
+++ shop-admin/src/main/java/com/gexingw/shop/mapper/PmsProductMapper.java
@@ -0,0 +1,9 @@
+package com.gexingw.shop.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.gexingw.shop.bean.pms.PmsProduct;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface PmsProductMapper extends BaseMapper<PmsProduct> {
+}
