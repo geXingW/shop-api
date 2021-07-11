4960cb07c4bc010e2af9d3c9922b4d47d467ed9d
WangSF <wolfs_9@hotmail.com>
2021-07-11 23:10:25 +0800

    :bug: 解决Admin认证问题

--- shop-security/src/main/java/com/gexingw/shop/config/WebSecurityConfig.java
+++ shop-security/src/main/java/com/gexingw/shop/config/WebSecurityConfig.java
@@ -1,10 +1,11 @@
 package com.gexingw.shop.config;
 
+import com.gexingw.shop.component.JwtAuthenticationTokenFilter;
+import com.gexingw.shop.component.RestfulAccessDeniedHandler;
 import com.gexingw.shop.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,14 +13,18 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-@Configuration
-@EnableWebSecurity
-@Order(-20)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     AuthService authService;
+
+    @Autowired
+    private JwtAuthenticationTokenFilter jwtAuthenticationTokenFilter;
+
+    @Autowired
+    private RestfulAccessDeniedHandler restfulAccessDeniedHandler;
 
     @Bean
     public PasswordEncoder passwordEncoder(){
@@ -27,20 +32,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService());
-    }
-
-    @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/").permitAll();
+        // 添加登陆认证拦截器
+        http.addFilterBefore(jwtAuthenticationTokenFilter, UsernamePasswordAuthenticationFilter.class);
 
-        super.configure(http);
-    }
-
-    @Bean
-    public UserDetailsService userDetailsService() {
-        return (String username) -> authService.loadUserByUsername(username);
+        // 设置拒绝访问的处理
+        http.exceptionHandling().accessDeniedHandler(restfulAccessDeniedHandler);
     }
 
 }
